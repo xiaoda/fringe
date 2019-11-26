341ad04561b435a2297c992464038bde4e46d278
xiaoda <lqdai.xiaoda@gmail.com>
2019-11-26 18:14:10 +0800

    Add changeRate for Pulse

--- pulse/pulse.js
+++ pulse/pulse.js
@@ -1,15 +1,27 @@
 class Pulse {
   constructor (options = {}) {
-    const rate = options.rate || 80
-    this.interval = 60 / rate
+    this.rate = options.rate || 80
     this.callback = null
     this.pulsing()
   }
 
+  changeRate (target, duration) {
+    target = Math.round(target)
+    const steps = target - this.rate
+    const interval = duration / Math.abs(steps) * 1000
+    const step = steps / Math.abs(steps)
+    const intervalID = GeometryUtils.setIntervalCustom(_ => {
+      this.rate += step
+      if (this.rate === target) clearInterval(intervalID)
+    }, interval)
+  }
+
   pulsing () {
-    GeometryUtils.setIntervalCustom(_ => {
+    const interval = 60 / this.rate
+    GeometryUtils.setTimeoutCustom(_ => {
+      this.pulsing()
       if (this.callback) this.callback()
-    }, this.interval * 1000)
+    }, interval * 1000)
   }
 
   onPulse (callback) {
