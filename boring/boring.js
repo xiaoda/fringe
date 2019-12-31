6b7f52da3697abc92ce09ef805a1ed64c363e0f3
xiaoda <lqdai.xiaoda@gmail.com>
2019-12-31 15:33:15 +0800

    Custom frontend framework v1

---
+++ boring/boring.js
@@ -0,0 +1,26 @@
+class Boring {
+  constructor (options) {
+    this.container = $(options.selector)
+    this.data = options.data || {}
+    this.methods = options.methods || {}
+    this.content = options.content || (_ => '')
+
+    if (!this.container.length) return
+    Object.keys(this.methods).forEach(key => {
+      this.methods[key] = this.methods[key].bind(this)
+    })
+    this.content = this.content.bind(this)
+    this.render()
+  }
+
+  setData (dataObject) {
+    Object.keys(dataObject).forEach(key => {
+      this.data[key] = dataObject[key]
+    })
+    this.render()
+  }
+
+  render () {
+    this.container.empty().html(this.content())
+  }
+}
