b8c057c67a1cd1448966baa9059edde511d53f77
xiaoda <lqdai.xiaoda@gmail.com>
2020-01-02 11:36:11 +0800

    Improve frontend component

--- component/component.js
+++ component/component.js
@@ -1,26 +1,60 @@
 class Component {
   constructor (options = {}) {
-    this.container = $(options.selector)
+    this.name = options.name
+    this.selector = options.selector
     this.data = options.data || {}
     this.methods = options.methods || {}
-    this.content = options.content || (_ => '')
+    this.render = options.render || (_ => '')
+    this.parent = null
+    this.parentDataAlreadySet = false
 
-    if (!this.container.length) return
     Object.keys(this.methods).forEach(key => {
-      this.methods[key] = this.methods[key].bind(this)
+      if (this[key]) return
+      this[key] = this.methods[key]
     })
-    this.content = this.content.bind(this)
-    this.render()
+    this.render = this.render.bind(this)
+    this.update()
+  }
+
+  getContainer () {
+    return $(this.selector)
   }
 
   setData (dataObject) {
     Object.keys(dataObject).forEach(key => {
       this.data[key] = dataObject[key]
     })
-    this.render()
+    this.update()
   }
 
-  render () {
-    this.container.empty().html(this.content())
+  setDataFromParent (dataObject) {
+    if (this.parentDataAlreadySet) return
+    this.parentDataAlreadySet = true
+    this.setData(dataObject)
+  }
+
+  setParent (component) {
+    this.parent = component
+  }
+
+  update () {
+    const container = this.getContainer()
+    if (container.length) {
+      setTimeout(_ => {
+        container.html(this.render())
+      }, 0)
+    } else if (this.parent) {
+      this.parent.update()
+    }
+  }
+
+  static sub (component, parentComponent, dataObject) {
+    if (component instanceof Component) {
+      component.setParent(parentComponent)
+      if (dataObject) component.setDataFromParent(dataObject)
+      return component.render()
+    } else if (typeof component === 'function') {
+      return component(dataObject)
+    }
   }
 }
