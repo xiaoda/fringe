8bbbe725068e387f0b8ef9abfdba0b951a1da1d0
xiaoda <lqdai.xiaoda@gmail.com>
2020-06-10 14:14:57 +0800

    Simplify cities component

--- transport/components/cities.js
+++ transport/components/cities.js
@@ -10,28 +10,19 @@ const $citiesComponent = new Component({
       <table border>
         <thead>
           <tr>
-            <th rowspan="3">City</th>
-            <th rowspan="3">Airports</th>
-            <th rowspan="3">Population</th>
+            <th rowspan="2">City</th>
+            <th rowspan="2">Airports</th>
+            <th rowspan="2">Population</th>
             <th
-              colspan="${Object.keys(cities).length * 2}"
+              colspan="${Object.keys(cities).length}"
             >
               Destinations
             </th>
           </tr>
           <tr>
             ${Object.keys(cities).map(name => `
-              <th colspan="2">${name}</th>
+              <th>${name}</th>
             `).join('')}
-          </tr>
-          <tr>
-            ${Object.keys(cities).map(name => {
-              const city = cities[name]
-              return `
-                <th>Current</th>
-                <th>Max</th>
-              `
-            }).join('')}
           </tr>
         </thead>
         <tbody>
@@ -46,10 +37,9 @@ const $citiesComponent = new Component({
                   const destCity = cities[destName]
                   return (
                     name === destName ?
-                    `<td colspan="2" class="no-data"></td>` :
+                    `<td class="no-data"></td>` :
                     `
                       <td>${city.getCurrentTravelPopulation(destCity)}</td>
-                      <td>${city.getTravelPopulation(destCity)}</td>
                     `
                   )
                 }).join('')}
